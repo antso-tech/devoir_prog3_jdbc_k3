d9e0a392f8be37ae2fc287ab807dfdf097532b6f
antso.tech <hei.antso.2007@gmail.com>
2026-03-09 09:13:15 +0300

    test avec dbConnection et ajout de la premiere classe

---
+++ backend/src/main/java/org/example/DBConnection.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    Connection getConnection(){
+        try{
+            Dotenv dotenv = Dotenv.load();
+            String url = dotenv.get("DB_URL");
+            String user = dotenv.get("DB_USER");
+            String password = dotenv.get("DB_PASSWORD");
+
+            return DriverManager.getConnection(url, user, password);
+        }catch(SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
--- backend/src/main/java/org/example/Main.java
+++ backend/src/main/java/org/example/Main.java
@@ -4,6 +4,8 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        DBConnection dbConnection = new DBConnection();
+        dbConnection.getConnection();
 
     }
 }
